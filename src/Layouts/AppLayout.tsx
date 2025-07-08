7d3ef637ab03300ca28622d34efc252e6a544743
Jonnathan23 <jsaquicela26@gmail.com>
2025-07-08 15:34:16 -0500

    Layout y pagina principal creadas

---
+++ src/Layouts/AppLayout.tsx
@@ -0,0 +1,16 @@
+
+export default function AppLayout() {
+    return (
+        <>
+            <header>
+                <h1>SmartCeramic Advisor</h1>
+            </header>
+            <div>
+                
+            </div>
+            <footer>
+                <p>Copyright &copy; 2025</p>
+            </footer>
+        </>
+    );
+}
