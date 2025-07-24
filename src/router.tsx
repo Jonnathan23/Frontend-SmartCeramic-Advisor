064360c1b8c697201df940dcf3bc7b2a34bc07c5
Jonnathan23 <jsaquicela26@gmail.com>
2025-07-24 13:30:20 -0500

    nombre actualizado

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./Layouts/AppLayout";
-import CeramicDetails from "./pagesss_temp/CeramicDetails";
+import CeramicDetails from "./pages/CeramicDetails";
 import NotFoundLayout from "./Layouts/NotFoundLayout";
 import AuthLayout from "./Layouts/AuthLayout";
 
